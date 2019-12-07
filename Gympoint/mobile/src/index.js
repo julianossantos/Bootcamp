9770a6b7d74095572229abf894100b7d4b8200a7
julianossantos <julianossantos@gmail.com>
2019-12-07 18:42:23 -0300

    gympoint mobile start

---
+++ Gympoint/mobile/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import Teste from '~/Teste';
+// import { Container } from './styles';
+
+export default function App() {
+  return <Teste />;
+}
